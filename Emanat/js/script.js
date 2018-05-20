60bf9efbbc8b0f95b6d9030de6faaa247d37871b
SoltanovElnur <elnures@code.edu.az>
2018-05-21 00:03:41 +0400

    Emanat

---
+++ Emanat/js/script.js
@@ -0,0 +1,11 @@
+function changePage(text){
+    document.location.href=text;
+}
+function clic(a) {
+    document.getElementById("in").value += a;
+  }
+  function clear(){
+    document.getElementById("in").value = "";
+      
+
+  }
